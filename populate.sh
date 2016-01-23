0d07ab6492f6264bad53c712483052b4e0fa92f8
metacollin <metacollin@gmail.com>
2016-01-23 15:08:30 -0700

    Fixed.

--- populate.sh
+++ populate.sh
@@ -1,4 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+git remote remove walter
+echo Added: walter
+git remote add walter git://smisioto.eu/kicad_libs.git
+git fetch walter
+
+git remote remove kicad-lib
+echo Added: kicad-lib
+git remote add kicad-lib https://github.com/KiCad/kicad-library.git  
+git fetch kicad-lib
+
+git merge walter/master
+mv library/power.dcm library/w_power.dcm
+mv library/power.lib library/w_power.lib
+mv library/conn.dcm library/w_conn.dcm
+mv library/conn.lib library/w_conn.lib
+
+git merge kicad-lib/master
 
 
 curl -s 'https://api.github.com/orgs/kicad/repos?page=1&per_page=100' \
@@ -6,14 +24,6 @@ curl -s 'https://api.github.com/orgs/kicad/repos?page=1&per_page=100' \
  | grep pretty | while read -r line ; do
  	git remote remove `basename $line .pretty.git`
  	echo Added: `basename $line .git`
- 	git remote add `basename $line .pretty.git` $line 
+ 	git remote add -f `basename $line .pretty.git` $line
+ 	git subtree add --prefix modules/`basename $line .git` `basename $line .pretty.git` master --squash
  done
-git remote remove walter
-echo Added: walter
-git remote add walter git://smisioto.eu/kicad_libs.git
-
-git remote remove kicad-lib
-echo Added: kicad-lib
-git remote add kicad-lib https://github.com/KiCad/kicad-library.git  
-
-git merge walter/master --message "Merging Walter"
