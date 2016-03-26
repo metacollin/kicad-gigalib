2142fa3082de8c631da1b93e8d1969cbc836cf0e
Collin Anderson <collinanderson@173-11-110-230-SFBA.hfc.comcastbusiness.net>
2016-03-26 05:36:45 -0700

    Now generates fp-lib-table

--- populate.sh
+++ populate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# #!/bin/bash
 
 git remote remove walter
 echo Added: walter
@@ -17,13 +17,23 @@ mv library/conn.dcm library/w_conn.dcm
 mv library/conn.lib library/w_conn.lib
 
 git merge kicad-lib/master
+touch fp-lib-table-backups
+cat fp-lib-table >> fp-lib-table-backups
 
-
-curl -s 'https://api.github.com/orgs/kicad/repos?page=1&per_page=100' \
+ FPLIBTABLESTRING='(fp_lib_table'
+ echo -e "$FPLIBTABLESTRING" > fp-lib-table
+ curl -s 'https://api.github.com/orgs/kicad/repos?page=1&per_page=100' \
  | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| puts repo["clone_url"] }' \
  | grep pretty | while read -r line ; do
  	git remote remove `basename $line .pretty.git`
  	echo Added: `basename $line .git`
  	git remote add -f `basename $line .pretty.git` $line
  	git subtree add --prefix modules/`basename $line .git` `basename $line .pretty.git` master --squash
- done
+ 	echo -e "  (lib (name `basename $line .pretty.git`)(type KiCad)(uri \${KISYSMOD}/`basename $line .git`(options "")(descr ""))" >> fp-lib-table
+ done 
+
+tail -n +2 fp-lib-table.walter >> fp-lib-table
+
+cat fp-lib-table
+
+echo "Done!"
